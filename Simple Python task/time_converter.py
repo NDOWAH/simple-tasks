f128053edd723a4e9c1e8c04a6c86e763ec2bdf3
Marcelgrossmann <marcelgrossmann80@gmail.com>
2021-07-21 02:54:05 +0100

    time formating

--- Simple Python task/time_converter.py
+++ Simple Python task/time_converter.py
@@ -1,6 +1,10 @@
-def my_time(seconds):
+def my_time():
     request = input("please type in your time in seconds: ")
-    hours = seconds//3600
-    minutes = (seconds % 60)// 60
-    seconds = seconds % 60
+    hours =request//3600
+    minutes = (request % 60)// 60
+    seconds = request % 60
     print("The time is %s:%s:%s"%(hours,minutes,seconds))
+
+
+time = my_time()
+print(time)
