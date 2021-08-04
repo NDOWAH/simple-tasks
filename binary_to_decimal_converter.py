fbf5831661dd4c94150944a730ea329b5c1a8b7b
NDOWAH <70651812+NDOWAH@users.noreply.github.com>
2021-08-04 21:03:50 +0100

    Add files via upload

--- binary_to_decimal_converter.py
+++ binary_to_decimal_converter.py
@@ -2,15 +2,18 @@ def decimal_binary(number):
     decimal_number = 0
     number = str(number)
     for digit in number:
-        break
-
-    if int(digit) > 1:
-        print("The number inputed is not a binary number, binary numbers contain only ones and zeros.")
-
-    else:
-        for i, digit in enumerate(str(number)):
-            decimal_number += int(digit)*2**int(i)
+        if int(digit) > 1:
+           print("The number inputed is not a binary number, binary numbers contain only ones and zeros.")
+        else:
+            index = ""
+            for i,  digit in enumerate(str(number)):
+               index += str(i)
+               break
+            new_list = index[::-1]
+            print(new_list)
+            for i in new_list:
+                   decimal_number += int(digit)*2**int(i)
     return f"The decimal equivalent of {number} is: {decimal_number}"
 
 
-print(decimal_binary(11111111111000001))
+
