b48bda1c293421f2a0ec4e8ae0d0bd7de2232abb
NDOWAH <70651812+NDOWAH@users.noreply.github.com>
2021-08-16 10:31:09 +0100

    Add files via upload

--- vectoralgebra.py
+++ vectoralgebra.py
@@ -1,4 +1,4 @@
-from math import acos, sqrt
+from math import acos, sqrt, pi
 class VectorAlg:
 
 
@@ -32,18 +32,30 @@ class VectorAlg:
         prod = [x*y for x,y in zip(self.coordinates, v.coordinates)]
         return sum(prod)
 
-    def angle(self,v):
-        dir1 = self.direction()
-        dir2 = v.direction()
-        d3 = dir1.dot(dir2)
-        in_radian = acos(dir1.dot(dir2))
-        return in_radian
+    def angle(self, v, in_degree = False):
+            dir1 = self.direction()
+            dir2 = v.direction()
+            d3 = dir1.dot(dir2)
+            in_radian = acos(dir1.dot(dir2))
+            if in_degree:
+                degrees_per_radian = 180/pi
+
+                return in_radian * in_radian
+    def is_orthogonal_to(self, v, tolerance = 1e-10):
+        return abs(self.dot(v)) < tolerance
+
+    def is_parallel_to(self, v):
+        return(self.is_zero()
+        or v.is_zero()
+        or self.angle_with(v)==0
+        or self.angle_with(v) == pi)
 
 
 
-v = VectorAlg([2,3])
+
+v = VectorAlg([9,3])
 w = VectorAlg([9,2])
-print(v.dot(w))
+print("the angle is: ", v.dot(w))
 # print(v.angle(w) 
 # print(v.magnitude())
 # print(v.scalar(4))
