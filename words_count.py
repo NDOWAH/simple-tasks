c1b2a79e12554bb7a056d7bdf27ecf5ae13471c6
NDOWAH <70651812+NDOWAH@users.noreply.github.com>
2021-08-19 22:03:15 +0100

    Add files via upload

---
+++ words_count.py
@@ -0,0 +1,8 @@
+def counter(object):
+    count = 0
+    for i in object:
+        count +=1
+    print("The total number of items are:", count)
+
+
+print(counter("they are two of them in the class"))
