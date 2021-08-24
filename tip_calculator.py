103b80209ade79b97dd4bb8dd32718ab11b4fc24
NDOWAH <70651812+NDOWAH@users.noreply.github.com>
2021-08-24 19:10:26 +0100

    Add files via upload

--- tip_calculator.py
+++ tip_calculator.py
@@ -3,5 +3,7 @@ def bill_split(bill):
     num_of_people = int(input("How many people should the bill be distributed to? "))
     percentage_split = int(input("What percentage tip would you like to give? 10, 12 or 15: "))
     bill = round((bill * (1 - percentage_split / 100)) / num_of_people, 3)
-    print(f"Each person will pay: ${bill}".format(bill))
+    return ("Each person will pay  ${0:.3f}".format(bill))
 
+
+print(bill_split(234))
