88247bf738c41e13c8cba4daf92cdafbabac87fc
NDOWAH <70651812+NDOWAH@users.noreply.github.com>
2021-08-26 19:06:57 +0100

    Add files via upload

--- prime_check.py
+++ prime_check.py
@@ -1,11 +1,12 @@
 def prime_checker(number):
-    for num in range(1,10):
+     for num in range(1,10):
         if number % num ==0:
-            pass
+            print("{0} is not a prime number".format(number))
         else:
-            print("{0} is a prime number.".format(number))
-            break
+          print("{0} is a prime number.".format(number))
+        break
+            
 #To be corrected
     
 
-print(prime_checker(4))
+print(prime_checker(3))
